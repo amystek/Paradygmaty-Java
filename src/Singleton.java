6b92d64ce5549b186bb8d26f030540de020e3f61
amystek <arkadiusz.mystek@gmail.com>
2019-12-12 13:24:45 +0100

    Added class Singleton

--- src/Singleton.java
+++ src/Singleton.java
@@ -13,7 +13,6 @@ public class Singleton {
 
     public static void main(String[] args) {
         Singleton s1 = new Singleton();
-        Singleton s2 = new Singleton();
         System.out.println(s1);
     }
 }
