f588c721248a9b816e590ec68fc7cda7998315a1
amystek <arkadiusz.mystek@gmail.com>
2019-12-12 18:22:12 +0100

    Added class Singleton

--- src/Singleton.java
+++ src/Singleton.java
@@ -1,18 +1,28 @@
 public class Singleton {
+    private volatile static Singleton instance;
+
+    public int i;
 
     private Singleton() {
     }
 
     public static Singleton getInstance() {
-        return SingletonHolder.INSTANCE;
-    }
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
 
-    private static class SingletonHolder {
-        private static final Singleton INSTANCE = new Singleton();
+        return instance;
     }
 
     public static void main(String[] args) {
-        Singleton s1 = new Singleton();
-        System.out.println(s1);
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        s1.i = 1;
+        System.out.println(s1.i);
+        System.out.println(s2.i);
     }
 }
