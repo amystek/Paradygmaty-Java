89cbdf4c0adf1e8edd1c137eb452d0c770cba5fc
amystek <arkadiusz.mystek@gmail.com>
2019-12-12 13:07:38 +0100

    Added class Singleton

---
+++ src/Singleton.java
@@ -0,0 +1,13 @@
+public class Singleton {
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+
+    private static class SingletonHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+}
